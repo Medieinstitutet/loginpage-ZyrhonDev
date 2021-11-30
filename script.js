fb7d1a47efee2651de9d6466b01b8103313b77cd
ZyrhonDev <93902196+ZyrhonDev@users.noreply.github.com>
2021-11-30 20:44:30 +0100

    forgot to push, added login & logout with dynamic views

--- script.js
+++ script.js
@@ -1,30 +1,90 @@
 // Create variables for login
 let userName = "janne";
+localStorage.setItem("myUsername", userName);
 let userPassword = "test";
-let loggedIn = false;
+localStorage.setItem("myPassword", userPassword);
 
 // Global content, create and append. 
 let navBar = document.createElement ("nav");
+const myDiv = document.getElementById ("root");
+const mainDiv = document.createElement ("div");
 let siteFooter = document.createElement ("footer");
 let navUl = document.createElement ("ul");
-document.body.insertAdjacentElement ("afterbegin", navBar);
-document.body.insertAdjacentElement ("beforeend", siteFooter);
+const myH = document.createElement ("h1");
+myH.innerText = "Zyrhon (Aka Oskar Lundberg)'s Login website."
+document.body.append (myDiv);
+myDiv.insertAdjacentElement ("beforebegin", myH);
+myDiv.insertAdjacentElement ("afterbegin", navBar);
+myDiv.insertAdjacentElement ("beforeend", siteFooter);
+myDiv.append (mainDiv);
+mainDiv.id = "mainDiv";
 navBar.append (navUl);
 
+// Welcome msg
+let welcomeMsg = document.createElement ("p");
+welcomeMsg.innerText = "Welcome to the site, please login to access all the juicy content brought to us here by Zyrhon."
+welcomeMsg.id = "whaleCum";
+mainDiv.append (welcomeMsg);
+// Login form
+const loginForm = document.createElement ("form");
+const accountInput = document.createElement ("input");
+accountInput.placeholder = "Account name"
+const passwordInput = document.createElement ("input");
+passwordInput.placeholder = "Password";
+passwordInput.type = "password";
+const loginBtn = document.createElement ("button");
+loginBtn.innerText = "Login";
+mainDiv.append (loginForm);
+loginForm.append (accountInput, passwordInput, loginBtn);
+
+const logOut = document.createElement ("li");
+// Create functionality to login
+loginBtn.addEventListener ("click", function() {
+    let getUsername = localStorage.getItem("myUsername");
+    let getPassword = localStorage.getItem("myPassword");
+    let getLoggedIn = localStorage.getItem("authenticatedUser");
+
+    if (accountInput.value == getUsername && passwordInput.value == getPassword) {
+        localStorage.setItem("authenticatedUser", true);
+        console.log("Login successful");
+        logOut.innerHTML = "<span class='navLinks'>Logout</span>";
+        logOut.id = "logOut";
+        navUl.append (logOut);
+        mainDiv.innerHTML = "";
+        
+        location.reload(); 
+    } else {
+        welcomeMsg.innerHTML = "Wrong account and/or password."
+        welcomeMsg.style = "color: red";
+    }
+})
 // Create view for users who are not logged in.
 // Nav with login option
 let myHome = document.createElement ("li");
 let myLogin = document.createElement ("li");
-myHome.innerHTML = "<span class='navLinks'>Home</span>";
-myLogin.innerHTML = "<span class='navLinks'>Login</span>";
+myHome.innerHTML = "<span class='navLinks'><a href='#'>Home</a></span>";
 navUl.append (myHome);
-navUl.append (myLogin);
 
 // Create view for logged in users.
 // Nav with logout option
+function loggedIn() {
+    let getLoggedIn = localStorage.getItem("authenticatedUser");
+    console.log(getLoggedIn);
+    if (getLoggedIn == "true") {
+        mainDiv.innerHTML = "";
+        logOut.innerHTML = "<span class='navLinks'>Logout</span>";
+        logOut.id = "logOut";
+        navUl.append (logOut);
+        mainDiv.append (welcomeMsg);
+        welcomeMsg.innerHTML = "You've successfully logged in, welcome to this super exlusive website!!"
+    }
+}
 
-
-
-
-// if statement for if user is logged in to show diff views
+// Click to logout
+logOut.addEventListener ("click", function() {
+    getLoggedIn = localStorage.getItem("authenticatedUser");
+    localStorage.setItem("authenticatedUser", false);
+    location.reload(); 
+})
+loggedIn();
 
